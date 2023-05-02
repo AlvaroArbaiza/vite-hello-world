97b86fb6f45f6505b94b512a378c1523a8efa3ec
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-02 17:29:23 +0200

    Creazione collegamento con vite e scrittura di un nuovo componente AppTitle

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  name: "App",
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <h1>La mia prima App con Vue!</h1>
+  <AppTitle />
+</template>
+
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: "AppTitle"
+}
+</script>
+
+<template>
+    <h2>Ciao come va?</h2>
+</template>
+
+<style scoped>
+h2 {
+    color: red;
+}
+</style>
